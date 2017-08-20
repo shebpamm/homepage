604cd205e1a04cef2c051012487b57ed440ee622
shebpamm <shebpamm123@gmail.com>
2017-08-20 20:35:17 +0000

    Made things liverefresh

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const sass = require('node-sass-middleware')
+var pug = require('pug');
+
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/base/dev/views');
+
 app.get('/', function(req, res) {
-  res.send('Hello World!')
+  res.render('index', {title: 'Hey', message: 'Terve pappa1
+  ' });
 })
 
 app.listen(8888, function() {
-  console.log('Server running on port 8888!')
+  console.log('Server running on port 8888!');
 })
+
+app.use(
+  sass({
+    src: __dirname + '/sass',
+    dest: __dirname + '/css',
+    debug: true
+  })
+)
